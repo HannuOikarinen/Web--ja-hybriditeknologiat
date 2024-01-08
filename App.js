78236a10f4fe8798c77e07bd0aaf8f8359e4417d
HannuOikarinen <oikarhan@gmail.com>
2024-01-08 14:38:48 +0200

    initial

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
-export default function App() {
+const HeartRateCalculator = () => {
+  const [age, setAge] = useState('');
+  const [lowerLimit, setLowerLimit] = useState('');
+  const [upperLimit, setUpperLimit] = useState('');
+
+  const calculateHeartRateLimits = (inputAge) => {
+    if (inputAge !== '') {
+      const parsedAge = parseInt(inputAge, 10);
+      const lower = Math.floor((220 - parsedAge) * 0.65);
+      const upper = Math.floor((220 - parsedAge) * 0.85);
+
+      setLowerLimit(lower.toString());
+      setUpperLimit(upper.toString());
+    } else {
+      setLowerLimit('');
+      setUpperLimit('');
+    }
+  };
+
+  const handleAgeChange = (text) => {
+    setAge(text);
+    calculateHeartRateLimits(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Enter your age:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Enter Age"
+        value={age}
+        onChangeText={handleAgeChange}
+      />
+      <View style={styles.limitsContainer}>
+        <Text style={styles.limitsText}>Lower Limit: {lowerLimit}</Text>
+        <Text style={styles.limitsText}>Upper Limit: {upperLimit}</Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'left',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  limitsContainer: {
+    alignItems: 'left',
+  },
+  limitsText: {
+    fontSize: 16,
+    marginBottom: 10,
   },
 });
+
+export default HeartRateCalculator;
